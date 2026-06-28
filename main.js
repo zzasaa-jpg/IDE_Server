aac2de41b1ca129f058e13e5cf749a0a0c3458d7
Mahendra Kumar <zzsdrt354@gmail.com>
2026-06-28 18:30:04 +0530

    feat: implement recursive filesystem fallback API.
    ~ Implemented the readDirectory function and read_directories API route.
    ~ Added recursive directory traversal for nested folders.
    ~ Unified the response structure between the browser provider and server provider.
    ~ Included file metadata (name, size, modified time).
    ~ Added CORS and Express JSON middleware.
    
    Signed-off-by: Mahendra Kumar <zzsdrt354@gmail.com>

--- main.js
+++ main.js
@@ -1,13 +1,54 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
 const app = express();
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
-
+app.use(express.json());
+app.use(cors());
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, ()=> {
+function readDirectory(dirPath) {
+	const children = [];
+	const files = fs.readdirSync(dirPath);
+
+	files.forEach(file => {
+		const fullPath = path.join(dirPath, file);
+		const stats = fs.statSync(fullPath);
+		if (stats.isDirectory()) {
+			children.push({
+				name: file,
+				type: "directory",
+				children: readDirectory(fullPath),
+			});
+		} else {
+			children.push({
+				name: file,
+				type: "file",
+				size: stats.size,
+				modified: stats.mtimes,
+			});
+		}
+	});
+	return children;
+}
+
+app.get('/read_directories', (req, res) => {
+	const dirPath = req.query.path;
+	console.log("dirPath -> ", dirPath);
+	let children = [];
+	try {
+		const tree = readDirectory(dirPath != undefined ? dirPath : "C:/Users/zzsdr/Desktop");
+		res.json({ tree, "dirParh": dirPath });
+	}
+	catch (err) {
+		console.error(err);
+	}
+});
+
+app.listen(3000, () => {
 	console.log("Server is running on port 3000");
 });
